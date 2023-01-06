2c757fdb695a1a6463e9606685c463d4b6d8bd91
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2023-01-06 15:23:24 +0700

    Update crawlController.php

--- app/Http/Controllers/crawlController.php
+++ app/Http/Controllers/crawlController.php
@@ -47,7 +47,7 @@ class crawlController extends Controller
             $file_headers = @get_headers($file);
 
            
-            dd($file_headers);
+            dd($file);
 
             if($file_headers[0] == 'HTTP/1.0 200 OK'){
 
