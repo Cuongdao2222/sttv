f0d135279f7ef5f41cd2f806a88caadeccd72a69
Cuongdao2222 <89830567+Cuongdao2222@users.noreply.github.com>
2023-02-21 14:31:12 +0700

    Update apps.blade.php

--- resources/views/frontend/layouts/apps.blade.php
+++ resources/views/frontend/layouts/apps.blade.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link href="{{ asset('images/logo.jpg') }}" rel="shortcut icon" type="image/x-icon">
+        <link href="{{ asset('images/logo1.jpg') }}" rel="shortcut icon" type="image/x-icon">
         @if(isset($meta))
         <title>{{ $meta->meta_title }}</title>
         <meta property="og:description" content="{{ $meta->meta_content }}" /> 
