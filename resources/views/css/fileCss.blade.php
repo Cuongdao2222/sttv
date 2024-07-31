d017373afb59e618473f8240d14cb3670b0e113c
cuong12121 <cuongdao12121@gmail.com>
2024-07-31 14:30:01 +0700

    Update fileCss.blade.php

--- resources/views/css/fileCss.blade.php
+++ resources/views/css/fileCss.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @push('js')
+<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
 <link rel="stylesheet" type="text/css" href="{{ asset('lined-textarea/jquery-linedtextarea.css') }}">
 <script src="{{ asset('lined-textarea/jquery-linedtextarea.js') }}"></script>
 @endpush
