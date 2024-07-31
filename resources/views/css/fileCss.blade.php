4f56ad925d0b502dbc6507f0f36e9a8276c465a0
cuong12121 <cuongdao12121@gmail.com>
2024-07-31 14:58:24 +0700

    Update fileCss.blade.php

--- resources/views/css/fileCss.blade.php
+++ resources/views/css/fileCss.blade.php
@@ -5,7 +5,7 @@
 
 <link rel="stylesheet" type="text/css" href="{{ asset('lined-textarea/jquery-linedtextarea.css') }}">
 <script src="{{ asset('lined-textarea/jquery-linedtextarea.js') }}"></script>
-<script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script>
+<!-- <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha384-nvAa0+6Qg9clwYCGGPpDQLVpLNn0fRaROjHqs13t4Ggj3Ez50XnGQqc/r8MhnRDZ" crossorigin="anonymous"></script> -->
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
 
