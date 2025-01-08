bd6eb3efbaa28ee59b10117f26ee31d96b8e53b1
cuong12121 <cuongdao12121@gmail.com>
2025-01-08 16:21:11 +0700

    Update lienhe.blade.php

--- resources/views/frontend/lienhe.blade.php
+++ resources/views/frontend/lienhe.blade.php
@@ -6,6 +6,7 @@
 
         <link rel="stylesheet" type="text/css" href="{{ asset('css/categories.css') }}"> 
          <link rel="stylesheet" type="text/css" href="{{ asset('css/dienmay.css') }}"> 
+         <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.3.5/jquery.fancybox.min.js"></script>
          <script src="https://cdn.jsdelivr.net/jquery.validation/1.15.1/jquery.validate.min.js"></script>
  @endpush  
 
