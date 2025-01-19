0e3feafd5c66587d1d84bca29794e0504a6cbdb6
NguyenNam12322 <118891426+NguyenNam12322@users.noreply.github.com>
2025-01-19 16:20:12 +0700

    Update lienhe.blade.php

--- resources/views/frontend/lienhe.blade.php
+++ resources/views/frontend/lienhe.blade.php
@@ -24,15 +24,19 @@
 
     
 <div class="category">
-    <h1>Liên Hệ</h1>
+    <div class="container">
+        <h1>Liên Hệ</h1>
+        @if(session()->has('success'))
+        <p>{{ session('success') }}</p>
+
+        @endif
+
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.877950066217!2d105.86306907576858!3d20.99752898884952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac059f749033%3A0x86d4bd61d20ad365!2zMzc1IFAuIE1pbmggS2hhaSwgVsSpbmggVHV5LCBIYWkgQsOgIFRyxrBuZywgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1737277623042!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+
+    </div>
+    
 </div>
 
-@if(session()->has('success'))
-<p>{{ session('success') }}</p>
-
-@endif
-
-<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.877950066217!2d105.86306907576858!3d20.99752898884952!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ac059f749033%3A0x86d4bd61d20ad365!2zMzc1IFAuIE1pbmggS2hhaSwgVsSpbmggVHV5LCBIYWkgQsOgIFRyxrBuZywgSMOgIE7hu5lpLCBWaeG7h3QgTmFt!5e0!3m2!1svi!2s!4v1737277623042!5m2!1svi!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
 
 
 @endsection
